76f386ec4fafafa107378e5644d18ed21ea524b5
jianglei <jianglei@360.cn>
2017-06-28 18:13:15 +0800

    update

--- Server/keepmydevices/models.py
+++ Server/keepmydevices/models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import orm
 
 from datetime import datetime
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100))
